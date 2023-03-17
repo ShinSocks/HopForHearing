799afaa90e9c811d0507af5b49134bb05c9353b6
ShinSocks <astorabaguette@gmail.com>
2023-03-17 19:29:00 -0400

    d

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("hellowowrld");
+var opened;
+opened = false;
+
+function dropDown () {
+    if(!opened){
+        opened = true;
+        document.getElementById("header").style.height = "75%";
+        document.getElementById("dropdown-content").style.display = "block";
+    }
+
+    else if(opened){
+        opened = false;
+        document.getElementById("header").style.height = "10%";
+        document.getElementById("dropdown-content").style.display = "none";
+    }
+    
+}
