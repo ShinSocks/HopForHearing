b578ad426fcd885f6b1cf5197a8977048d2c9362
ShinSocks <astorabaguette@gmail.com>
2023-03-18 14:47:56 -0400

    k

--- index.js
+++ index.js
@@ -6,12 +6,15 @@ function dropDown () {
         opened = true;
         document.getElementById("header").style.height = "75%";
         document.getElementById("dropdown-content").style.display = "block";
+        document.getElementById("hamLogo").src = "./assets/x.png"
+
     }
 
     else if(opened){
         opened = false;
         document.getElementById("header").style.height = "10%";
         document.getElementById("dropdown-content").style.display = "none";
+        document.getElementById("hamLogo").src = "./assets/hamburger.png"
     }
     
 }
